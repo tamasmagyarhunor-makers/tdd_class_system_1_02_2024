aa44796eebda8cd8f65c4af5343284d13c1c5713
Magyar-Hunor Tamas <magyar-hunor.tamas@makers.tech>
2024-02-01 10:58:24 +0000

    Add add_items_to_backpack tests, pass

--- lib/person.py
+++ lib/person.py
@@ -1,3 +1,6 @@
 class Person():
     def __init__(self):
         self.backpack = []
+
+    def add_item_to_backpack(self, item):
+        self.backpack.append(item)
--- tests/test_person.py
+++ tests/test_person.py
@@ -7,3 +7,22 @@ def test_person_instantiates_with_backpack():
     expected = []
 
     assert actual == expected
+
+def test_person_puts_book_in_backpack():
+    person = Person()
+    person.add_item_to_backpack("book")
+
+    actual = person.backpack
+    expected = ["book"]
+
+    assert actual == expected
+
+def test_person_puts_book_and_phone_in_backpack():
+    person = Person()
+    person.add_item_to_backpack("book")
+    person.add_item_to_backpack("phone")
+
+    actual = person.backpack
+    expected = ["book", "phone"]
+
+    assert actual == expected
